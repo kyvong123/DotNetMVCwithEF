619ac5008f177b14b9ffeb3a675d16e57c79bf41
Le Ky vong <lekyvong123@gmail.com>
2021-11-22 06:39:19 +0700

    Update DataTable

--- WebApplication1/WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -30,6 +30,11 @@ namespace WebApplication1.Controllers
             return View();
         }
 
+        public IActionResult DataTableLayout()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
